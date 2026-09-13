a344b298b0e0a07882f896fa86f22da264ae28c8
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-13 12:04:12 +0530

    ini

---
+++ Plottings/Plot.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data ={
+    "Temp" : [50,40,44,44,45,42,23,20,12],
+    "Humidity" : [20,12,13,11,20,34,5,12,3],
+    "Wind" : [2,3,4,5,6,7,8,12,10],
+    "Precipitation" : [9,8,7,6,4,5,2,3,1]
+}
+
+df = pd.DataFrame(data)
+
+df.plot()
+
+plt.show()
